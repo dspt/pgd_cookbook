d6dd337a63f51c43e013e2a7437e63712bb834a9
Daniel Takamori <pono@osuosl.org>
2014-07-07 10:52:51 -0700

    ordering matters?

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,7 +9,7 @@ default['pgd']['github_user'] = "git"
 default['pgd']['install_dir'] = '/var/www/pgd'
 default['pgd']['config_dir'] = '/var/www/pgd/config'
 
-default['python']['prefix_dir'] = "/opt/python"
 default['python']['install_method'] = "source"
+default['python']['prefix_dir'] = "/opt/python"
 default['python']['version'] = "2.7.1"
 default['python']['checksum'] = "80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292"
