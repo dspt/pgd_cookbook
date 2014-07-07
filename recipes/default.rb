0fa44f02e8418e744d8e3ab070cc1d20346477f3
Daniel Takamori <pono@osuosl.org>
2014-07-07 11:30:51 -0700

    test changes and got python working

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,16 +14,6 @@ end
 # clone the repo 
 git node['pgd']['path'] do
   repository node['pgd']['repository']
-  revision node['pgd']['revision']
   user node['pgd']['owner']
   group node['pgd']['group']
 end
-
-
-directory "/tmp/folder" do
-  owner "root"
-  group "root"
-  mode 0755
-  action :create
-end
-
--- test/integration/default/serverspec/localhost/main.rb
+++ test/integration/default/serverspec/localhost/main.rb
@@ -1,30 +1,11 @@
 require 'spec_helper'
 
-describe package('git') do
-    it { should be_installed }
-end
-
-describe port(80) do
-  it { should_not be_listening }
-end
-
-describe service('sshd') do
-  it { should be_enabled }
-end
-
-describe file('/tmp/folder') do
-  it { should be_directory }
-  it { should be_mode 755 }
-  it { should be_owned_by 'root' }
-  it { should be_grouped_into 'root' }
-end
-
 %w{autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build byacc cscope ctags cvs diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap  zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel  mysql mysql-server MySQL-python mysql-connector-odbc git python-devel mysql-devel libffi-devel liberation-sans-fonts }.each do |p| 
     describe package(p) do 
         it { should be_installed}
     end
 end
 
-describe command('python2.7 --version') do
+describe command('/opt/python/bin/python2.7 --version') do
   it { should  return_stdout 'Python 2.7.1' }
 end
