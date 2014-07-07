6d808b94aaa6254f829f427b8ffe73b4baf4a1f6
Daniel Takamori <pono@osuosl.org>
2014-07-07 15:15:25 -0700

    maybe need virtualenv attr

--- attributes/default.rb
+++ attributes/default.rb
@@ -16,5 +16,6 @@ default['python']['prefix_dir'] = "/opt/python"
 default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
 default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
+default['python']['virtualenv_location'] = "#{node['python']['prefix_dir']}/bin/virtualenv"
 default['python']['version'] = "2.7.1"
 default['python']['checksum'] = "80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292"
