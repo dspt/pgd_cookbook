7f913334501cf044afdc686d0612d8f80a7a4d0b
Daniel Takamori <pono@osuosl.org>
2014-07-07 11:09:32 -0700

    why arent u in /opt/python?!

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,5 +11,7 @@ default['pgd']['config_dir'] = '/var/www/pgd/config'
 
 default['python']['install_method'] = "source"
 default['python']['prefix_dir'] = "/opt/python"
+default['python']['url'] = 'http://www.python.org/ftp/python'
+default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
 default['python']['version'] = "2.7.1"
 default['python']['checksum'] = "80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292"
