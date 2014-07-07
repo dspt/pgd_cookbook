9940040db7d7d5556c6dc2f5f8a2ffa6378254f0
Daniel Takamori <pono@osuosl.org>
2014-07-07 14:41:44 -0700

    trying to get pip

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['pgd']['path'] = "/var/www/pgd"
-default['pgd']['user'] = nil
+default['pgd']['user'] = 'pgd'
 default['pgd']['group'] = nil
 
 default['pgd']['repository'] = "git://git.osuosl.org/gitolite/pgd.git"
@@ -13,5 +13,6 @@ default['python']['install_method'] = "source"
 default['python']['prefix_dir'] = "/opt/python"
 default['python']['url'] = 'http://www.python.org/ftp/python'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+default['python']['pip_location'] = "#{node['python']['prefix_dir']}/bin/pip"
 default['python']['version'] = "2.7.1"
 default['python']['checksum'] = "80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292"
