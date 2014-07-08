6375f48cd177edb97ac43a4cff52a166ca74960e
Daniel Takamori <pono@osuosl.org>
2014-07-08 09:49:54 -0700

    first try at template

--- recipes/default.rb
+++ recipes/default.rb
@@ -30,3 +30,25 @@ python_pip node['pgd']['requirements.txt'] do
   virtualenv node['pgd']['virtualenv']
   action :install
 end
+
+settings = Chef::EncryptedDataBagItem.load("pgd", "settings")
+
+config_file = ::File.join(node['pgd']['config_dir'], 'settings.py')
+template config_file do
+  source "settings.py.erb"
+  owner node['pgd']['user']
+  group node['pgd']['group']
+  mode "0644"
+  variables({
+    :database_engine => database_engine,
+    :database_name => database_name,
+    :database_user => database_user,
+    :database_password => database_password,
+    :database_host => database_host,
+    :database_port => database_port,
+    :secret_key =>  secret_key,
+    :google_id => google_id;
+  })
+end
+
+
