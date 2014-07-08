a69dd422e496d020d4452d5345d4725aded51707
Daniel Takamori <pono@osuosl.org>
2014-07-08 11:34:28 -0700

    bad test

--- recipes/default.rb
+++ recipes/default.rb
@@ -69,8 +69,8 @@ end
 django_admin = ::File.join(node['pgd']['virtualenv'], '/bin/django-admin.py')
 django_version = `#{django_admin} --version`
 
-case django_version
-  when "1.3.7"
-    log django_version
-  when !"1.3.7"
+if django_version == "1.3.7"
+  log "1.3.7 ********************"
+else
+  log " NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOPE "
 end
