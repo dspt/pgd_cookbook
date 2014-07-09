cc87eb41eeff7667be66cf423affd2da9407f498
Jack Twilley <jmt@osuosl.org>
2014-07-09 16:01:53 -0700

    Added phantomjs recipe to kitchen, and test to main_spec
    The phantomjs recipe needed to be in .kitchen.yml for the recipe to be
    run, and a test was added to determine whether the recipe was run
    correctly.
    
    References #4.

--- test/integration/default/serverspec/localhost/main_spec.rb
+++ test/integration/default/serverspec/localhost/main_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-%w{autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build byacc cscope ctags cvs diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap  zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel  mysql mysql-server MySQL-python mysql-connector-odbc git python-devel mysql-devel libffi-devel liberation-sans-fonts }.each do |p| 
+%w{autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build byacc cscope ctags cvs diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap  zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel  mysql mysql-server MySQL-python mysql-connector-odbc git python-devel mysql-devel libffi-devel liberation-sans-fonts phantomjs}.each do |p| 
     describe package(p) do 
         it { should be_installed}
     end
