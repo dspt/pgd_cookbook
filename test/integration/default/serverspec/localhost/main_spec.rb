1cbf9c726b64a9cac7fa7013fab8f03639b2720e
Jack Twilley <jmt@osuosl.org>
2014-07-09 16:53:27 -0700

    Replaced test with one that works!
    Checked for presence of actual binary instead of package that doesn't
    exist.

--- test/integration/default/serverspec/localhost/main_spec.rb
+++ test/integration/default/serverspec/localhost/main_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-%w{autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build byacc cscope ctags cvs diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap  zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel  mysql mysql-server MySQL-python mysql-connector-odbc git python-devel mysql-devel libffi-devel liberation-sans-fonts phantomjs}.each do |p| 
+%w{autoconf automake binutils bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build byacc cscope ctags cvs diffstat doxygen elfutils gcc-gfortran git indent intltool patchutils rcs subversion swig systemtap  zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel  mysql mysql-server MySQL-python mysql-connector-odbc git python-devel mysql-devel libffi-devel liberation-sans-fonts}.each do |p| 
     describe package(p) do 
         it { should be_installed}
     end
@@ -12,6 +12,10 @@ end
 
 describe command('/var/www/pgd/.venv/bin/python --version') do
   it { should  return_stdout 'Python 2.7.1' }
+end
+
+describe command('ls /usr/local/bin/phantomjs') do
+  it { should return_exit_status 0 }
 end
 
 describe command('grep cairocffi /var/www/pgd/pgd/pgd_search/*/*.py|wc -l') do
